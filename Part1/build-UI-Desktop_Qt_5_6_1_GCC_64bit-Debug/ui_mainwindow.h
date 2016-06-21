731dd26f4e4cc792246ac1522da3ca5c541e2315
duong4996 <badboy.hp96@gmail.com>
2016-06-21 10:25:30 +0700

    Final part1

--- Part1/build-UI-Desktop_Qt_5_6_1_GCC_64bit-Debug/ui_mainwindow.h
+++ Part1/build-UI-Desktop_Qt_5_6_1_GCC_64bit-Debug/ui_mainwindow.h
@@ -17,6 +17,7 @@
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
@@ -31,7 +32,6 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QLabel *label;
-    QPushButton *pushButton;
     QTextEdit *DIR;
     QFrame *line;
     QLabel *label_2;
@@ -67,7 +67,9 @@ public:
     QTextEdit *play;
     QPushButton *pushButton_10;
     QPushButton *pushButton_11;
+    QPushButton *pushButton;
     QMenuBar *menuBar;
+    QMenu *menuUI;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
@@ -78,15 +80,14 @@ public:
         MainWindow->resize(1226, 656);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        centralWidget->setStyleSheet(QStringLiteral("background-color: rgb(169, 231, 117);"));
         label = new QLabel(centralWidget);
         label->setObjectName(QStringLiteral("label"));
         label->setGeometry(QRect(30, 0, 631, 17));
-        pushButton = new QPushButton(centralWidget);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(20, 30, 99, 31));
         DIR = new QTextEdit(centralWidget);
         DIR->setObjectName(QStringLiteral("DIR"));
         DIR->setGeometry(QRect(130, 30, 1051, 31));
+        DIR->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
         line = new QFrame(centralWidget);
         line->setObjectName(QStringLiteral("line"));
         line->setGeometry(QRect(0, 80, 271, 16));
@@ -112,33 +113,47 @@ public:
         pushButton_2 = new QPushButton(centralWidget);
         pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
         pushButton_2->setGeometry(QRect(20, 180, 99, 31));
+        pushButton_2->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
+        pushButton_2->setAutoDefault(false);
         label_6 = new QLabel(centralWidget);
         label_6->setObjectName(QStringLiteral("label_6"));
         label_6->setGeometry(QRect(20, 220, 101, 17));
         pushButton_3 = new QPushButton(centralWidget);
         pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
         pushButton_3->setGeometry(QRect(20, 240, 99, 31));
+        pushButton_3->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         pushButton_4 = new QPushButton(centralWidget);
         pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
         pushButton_4->setGeometry(QRect(20, 280, 99, 31));
+        pushButton_4->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         pushButton_5 = new QPushButton(centralWidget);
         pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
         pushButton_5->setGeometry(QRect(20, 320, 99, 31));
+        pushButton_5->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         label_7 = new QLabel(centralWidget);
         label_7->setObjectName(QStringLiteral("label_7"));
         label_7->setGeometry(QRect(10, 360, 131, 17));
         pushButton_6 = new QPushButton(centralWidget);
         pushButton_6->setObjectName(QStringLiteral("pushButton_6"));
         pushButton_6->setGeometry(QRect(20, 380, 99, 31));
+        pushButton_6->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         label_8 = new QLabel(centralWidget);
         label_8->setObjectName(QStringLiteral("label_8"));
         label_8->setGeometry(QRect(30, 420, 71, 17));
         pushButton_7 = new QPushButton(centralWidget);
         pushButton_7->setObjectName(QStringLiteral("pushButton_7"));
         pushButton_7->setGeometry(QRect(20, 440, 99, 31));
+        pushButton_7->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         list_video = new QTextEdit(centralWidget);
         list_video->setObjectName(QStringLiteral("list_video"));
         list_video->setGeometry(QRect(160, 150, 621, 391));
+        list_video->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
         line_3 = new QFrame(centralWidget);
         line_3->setObjectName(QStringLiteral("line_3"));
         line_3->setGeometry(QRect(790, 90, 20, 511));
@@ -164,18 +179,24 @@ public:
         name = new QTextEdit(centralWidget);
         name->setObjectName(QStringLiteral("name"));
         name->setGeometry(QRect(880, 150, 331, 31));
+        name->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
         pushButton_8 = new QPushButton(centralWidget);
         pushButton_8->setObjectName(QStringLiteral("pushButton_8"));
         pushButton_8->setGeometry(QRect(920, 190, 99, 31));
+        pushButton_8->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         pushButton_9 = new QPushButton(centralWidget);
         pushButton_9->setObjectName(QStringLiteral("pushButton_9"));
         pushButton_9->setGeometry(QRect(1060, 190, 99, 31));
+        pushButton_9->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         label_13 = new QLabel(centralWidget);
         label_13->setObjectName(QStringLiteral("label_13"));
         label_13->setGeometry(QRect(810, 230, 61, 17));
         searchResult = new QTextEdit(centralWidget);
         searchResult->setObjectName(QStringLiteral("searchResult"));
         searchResult->setGeometry(QRect(880, 230, 331, 161));
+        searchResult->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
         label_14 = new QLabel(centralWidget);
         label_14->setObjectName(QStringLiteral("label_14"));
         label_14->setGeometry(QRect(820, 400, 401, 17));
@@ -191,16 +212,28 @@ public:
         play = new QTextEdit(centralWidget);
         play->setObjectName(QStringLiteral("play"));
         play->setGeometry(QRect(910, 460, 301, 31));
+        play->setStyleSheet(QStringLiteral("background-color: rgb(255, 255, 255);"));
         pushButton_10 = new QPushButton(centralWidget);
         pushButton_10->setObjectName(QStringLiteral("pushButton_10"));
-        pushButton_10->setGeometry(QRect(940, 500, 99, 31));
+        pushButton_10->setGeometry(QRect(930, 500, 99, 31));
+        pushButton_10->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         pushButton_11 = new QPushButton(centralWidget);
         pushButton_11->setObjectName(QStringLiteral("pushButton_11"));
-        pushButton_11->setGeometry(QRect(1080, 500, 99, 31));
+        pushButton_11->setGeometry(QRect(1060, 500, 99, 31));
+        pushButton_11->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(20, 30, 99, 31));
+        pushButton->setStyleSheet(QLatin1String("background-color: rgb(123, 104, 104);\n"
+"color: rgb(255, 255, 255);"));
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 1226, 25));
+        menuUI = new QMenu(menuBar);
+        menuUI->setObjectName(QStringLiteral("menuUI"));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -208,6 +241,8 @@ public:
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuUI->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -218,11 +253,13 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
         label->setText(QApplication::translate("MainWindow", "Firstly, click Directory button to access to the for search, or type the directory into box", 0));
-        pushButton->setText(QApplication::translate("MainWindow", "Directory", 0));
         label_2->setText(QApplication::translate("MainWindow", "List and sort video", 0));
         label_3->setText(QApplication::translate("MainWindow", "Press any button due to your purpose. If the folder doesn't have any video then the box is empty. ", 0));
         label_4->setText(QApplication::translate("MainWindow", "With sort by created time, list all videos before sort", 0));
         label_5->setText(QApplication::translate("MainWindow", "List all video in folder", 0));
+#ifndef QT_NO_WHATSTHIS
+        pushButton_2->setWhatsThis(QApplication::translate("MainWindow", "<html><head/><body><p>List button</p></body></html>", 0));
+#endif // QT_NO_WHATSTHIS
         pushButton_2->setText(QApplication::translate("MainWindow", "List", 0));
         label_6->setText(QApplication::translate("MainWindow", "Sort video by:", 0));
         pushButton_3->setText(QApplication::translate("MainWindow", "Name", 0));
@@ -245,6 +282,8 @@ public:
         label_17->setText(QApplication::translate("MainWindow", "Directory box:", 0));
         pushButton_10->setText(QApplication::translate("MainWindow", "Play", 0));
         pushButton_11->setText(QApplication::translate("MainWindow", "Clear", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Directory", 0));
+        menuUI->setTitle(QApplication::translate("MainWindow", "UI", 0));
     } // retranslateUi
 
 };
